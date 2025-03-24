8caf8c9ec82da97d73aafc32b178603508acba04
Hardik <hardik@Ubuntu.myguest.virtualbox.org>
2025-03-23 22:19:07 -0600

    updated aesd-assignments.mk to use newer aesdsocket application

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 9a77af6977cc53000a56b2c03d432d38f55a470f
+AESD_ASSIGNMENTS_VERSION = d2d7d90003148aad181b3ee161d4ebc61c4fa9c9
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
